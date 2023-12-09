49bee2e141e9479dcae959cd5e14a10dd0450c63
Antti Kivi <antti.kivi@visiosto.fi>
2023-12-09 23:27:10 +0200

    feat(2023): add solution for the first part of day 9

--- 2023/day09/main.go
+++ 2023/day09/main.go
@@ -2,9 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/anttikivi/advent-of-code/2023/utils"
 )
+
+func isZero(nums []int) bool {
+	for _, num := range nums {
+		if num != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func extrapolate(nums []int) int {
+	var rows [][]int
+	i := 0
+	rows = append(rows, nums)
+	for !isZero(rows[i]) {
+		row := rows[i]
+		var next []int
+		end := len(row) - 1
+		for j := 0; j < end; j++ {
+			if j < end {
+				next = append(next, row[j+1]-row[j])
+			}
+		}
+		fmt.Println(next)
+		rows = append(rows, next)
+		i += 1
+	}
+	rows[i] = append(rows[i], 0)
+	for i > 0 {
+		len := len(rows[i])
+		rows[i-1] = append(rows[i-1], rows[i-1][len-1]+rows[i][len-1])
+		fmt.Println(rows[i])
+		i -= 1
+	}
+	fmt.Println(rows[0])
+	return rows[0][len(rows[0])-1]
+}
 
 func main() {
 	fmt.Println("Advent of Code 2023, Day 9")
@@ -13,4 +53,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	c := 0
+
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		nums := make([]int, len(fields))
+		for i, field := range fields {
+			nums[i], err = strconv.Atoi(field)
+			if err != nil {
+				panic(err)
+			}
+		}
+		fmt.Println(nums)
+		c += extrapolate(nums)
+	}
+
+	fmt.Println("Part 1: the sum of the extrapolated values is", c)
 }
