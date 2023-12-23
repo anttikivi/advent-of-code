6e33ff57c61d3bc1438688c2156241e7891c7786
Antti Kivi <antti.kivi@visiosto.fi>
2023-12-23 23:02:15 +0200

    feat(2023): add a solution for the second part of day 23

--- 2023/day23/main.go
+++ 2023/day23/main.go
@@ -6,7 +6,19 @@ import (
 	"time"
 
 	"github.com/anttikivi/advent-of-code/2023/utils"
+	mapset "github.com/deckarep/golang-set/v2"
+	"github.com/emirpasic/gods/queues/arrayqueue"
 )
+
+type pointAndLength struct {
+	P   image.Point
+	Len int
+}
+
+type Graph [142][142]int
+
+var graph Graph
+var maxID int
 
 // I suspect there is going to be many visited spots so the constant-time
 // lookup in maps is probably better than using a slice.
@@ -66,6 +78,30 @@ func findPaths(m map[image.Point]rune, w, h int, visited map[image.Point]bool, p
 	return longest
 }
 
+func isBitSet(bitSet, bitPos int) bool {
+	return (bitSet & (1 << bitPos)) != 0
+}
+
+func setBit(bitSet, bitPos int) int {
+	return bitSet | (1 << bitPos)
+}
+
+func searchDepthFirst(current, prev, end int, visited int) int {
+	if current == end {
+		return 0
+	}
+	maxi := -1
+	for j := 1; j < maxID; j++ {
+		if graph[current][j] != 0 && j != prev && !isBitSet(visited, j) {
+			d := searchDepthFirst(j, current, end, setBit(visited, j))
+			if d != -1 {
+				maxi = max(maxi, d+graph[current][j])
+			}
+		}
+	}
+	return maxi
+}
+
 func main() {
 	fmt.Println("*** Advent of Code 2023 ***")
 	fmt.Println("--- Day 23: A Long Walk ---")
@@ -90,6 +126,70 @@ func main() {
 
 	fmt.Println("Part 1: the longest hike is", hike, "steps")
 
-	elapse := time.Since(start)
-	fmt.Println("Part 1 ran for", elapse)
+	elapsed := time.Since(start)
+	fmt.Println("Part 1 ran for", elapsed)
+
+	start = time.Now()
+
+	w := len(lines[0])
+	h := len(lines)
+	sp := image.Pt(1, 0)
+	ep := image.Pt(w-2, h-1)
+	adjacents := make(map[image.Point][]image.Point)
+	nodes := make(map[image.Point]int)
+	nodes[sp] = 1
+	nodes[ep] = 2
+	id := 3
+	ds := []image.Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			p := image.Pt(x, y)
+			if p.X < 0 || p.X >= w || p.Y < 0 || p.Y >= h || m[p] == '#' {
+				continue
+			}
+
+			adjacents[p] = make([]image.Point, 0)
+			for _, d := range ds {
+				np := p.Add(d)
+				if np.X < 0 || np.X >= w || np.Y < 0 || np.Y >= h || m[np] == '#' {
+					continue
+				}
+				adjacents[p] = append(adjacents[p], np)
+			}
+			if len(adjacents[p]) > 2 {
+				nodes[p] = id
+				id += 1
+			}
+		}
+	}
+	maxID = id
+
+	graph = Graph{}
+	for p := range nodes {
+		id, queue, visited := nodes[p], arrayqueue.New(), mapset.NewSet[image.Point]()
+		queue.Enqueue(pointAndLength{p, 1})
+		visited.Add(p)
+
+		for !queue.Empty() {
+			pwl, _ := queue.Dequeue()
+			p, distance := pwl.(pointAndLength).P, pwl.(pointAndLength).Len
+			for _, neighbor := range adjacents[p] {
+				if !visited.Contains(neighbor) {
+					if nid, has := nodes[neighbor]; has {
+						graph[id][nid] = distance
+						graph[nid][id] = distance
+					} else {
+						queue.Enqueue(pointAndLength{neighbor, distance + 1})
+					}
+					visited.Add(neighbor)
+				}
+			}
+		}
+	}
+
+	hike = searchDepthFirst(1, 1, 2, 0)
+
+	fmt.Println("Part 2: the longest hike is", hike, "steps")
+	elapsed = time.Since(start)
+	fmt.Println("Part 2 ran for", elapsed)
 }
--- 2023/go.mod
+++ 2023/go.mod
@@ -1,3 +1,8 @@
 module github.com/anttikivi/advent-of-code/2023
 
 go 1.21.4
+
+require (
+	github.com/deckarep/golang-set/v2 v2.5.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+)
