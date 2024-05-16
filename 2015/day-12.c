9a265c036a69175682f31d4bca1102d84a21b151
Antti Kivi <antti.kivi@visiosto.fi>
2024-05-17 00:53:27 +0300

    feat(2015): add solution for part 2 of day 12

--- 2015/day-12.c
+++ 2015/day-12.c
@@ -1,8 +1,39 @@
+// cJSON is licensed under MIT License. See: https://github.com/DaveGamble/cJSON
+#include "utils/cJSON.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+#define BUFFER_LENGTH 8
+
+int get_sum(cJSON* json) {
+  int sum = 0;
+
+  if (json->type == cJSON_Object) {
+    cJSON* child = json->child;
+    while (child != NULL) {
+      if (child->valuestring != NULL &&
+          strcmp(child->valuestring, "red") == 0) {
+        return 0;
+      }
+      child = child->next;
+    }
+  }
+
+  cJSON* child = json->child;
+  while (child != NULL) {
+    if (child->type == cJSON_Object || child->type == cJSON_Array) {
+      sum += get_sum(child);
+    } else if (child->type == cJSON_Number) {
+      sum += child->valueint;
+    }
+    child = child->next;
+  }
+
+  return sum;
+}
 
 int main(void) {
   printf("\n");
@@ -10,7 +41,7 @@ int main(void) {
   printf("--- Day 12: JSAbacusFramework.io ---\n");
   printf("\n");
 
-  FILE* fp = fopen("input/day-12.txt", "r");
+  FILE* fp = fopen("input/day-12.txt", "rb");
 
   if (!fp) {
     perror("File opening failed");
@@ -20,7 +51,7 @@ int main(void) {
   clock_t start = clock();
 
   int sum = 0;
-  char buffer[8];
+  char buffer[BUFFER_LENGTH];
   int len = 0;
   char c;
 
@@ -37,8 +68,6 @@ int main(void) {
     }
   } while (c != EOF);
 
-  fclose(fp);
-
   printf("Part 1: the sum of the numbers in the file is %d\n", sum);
 
   clock_t end = clock();
@@ -48,9 +77,52 @@ int main(void) {
 
   printf("\n");
 
+  rewind(fp);
+
   start = clock();
 
-  printf("Part 2: Santa's next password is %s\n", "");
+  int ret = fseek(fp, 0, SEEK_END);
+  if (ret) {
+    perror("seeking the end of the file failed");
+    return EXIT_FAILURE;
+  }
+
+  len = ftell(fp);
+  if (len < -1) {
+    perror("getting the file position indicator failed");
+    return EXIT_FAILURE;
+  }
+
+  ret = fseek(fp, 0, SEEK_SET);
+  if (ret) {
+    perror("seeking the beginning of the file failed");
+    return EXIT_FAILURE;
+  }
+
+  char* jsonbuf = (char*)malloc(len);
+  if (!jsonbuf) {
+    perror("allocating the JSON buffer failed");
+    return EXIT_FAILURE;
+  }
+
+  ret = fread(jsonbuf, 1, len, fp);
+  if (ret != len && ferror(fp)) {
+    perror("reading the file into the buffer failed");
+    return EXIT_FAILURE;
+  }
+
+  cJSON* json = cJSON_Parse(jsonbuf);
+  if (json == NULL) {
+    printf("error parsing JSON: %s\n", cJSON_GetErrorPtr());
+    return EXIT_FAILURE;
+  }
+
+  sum = get_sum(json);
+
+  cJSON_Delete(json);
+  fclose(fp);
+
+  printf("Part 2: the sum of the numbers is %d\n", sum);
 
   end = clock();
   elapsed = (double)(end - start) / CLOCKS_PER_SEC;
