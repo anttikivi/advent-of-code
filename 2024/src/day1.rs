e0ad9ac30e68c78bd8604998e8e9813f1e4e9116
Antti Kivi <antti@anttikivi.fi>
2024-12-01 19:31:14 +0200

    feat(2024): add solution for the second part of the first day

--- 2024/src/day1.rs
+++ 2024/src/day1.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -12,10 +13,12 @@ pub fn run() {
     let input_file = File::open("input/day-01.txt").expect("failed to read the input file");
     let reader = BufReader::new(input_file);
 
+    let mut lines: Vec<String> = Vec::with_capacity(1000);
     let mut left_nums: Vec<i32> = Vec::with_capacity(1000);
     let mut right_nums: Vec<i32> = Vec::with_capacity(1000);
 
     for line in reader.lines().filter_map(|result| result.ok()) {
+        lines.push(line.clone());
         let (left_s, right_s) = line.split_once(SEPARATOR).unwrap();
         let left = left_s.to_string().parse::<i32>().unwrap();
         let right = right_s.to_string().parse::<i32>().unwrap();
@@ -33,4 +36,23 @@ pub fn run() {
     }
 
     println!("Part 1: the total distance between the two lists is {sum}");
+
+    left_nums = Vec::with_capacity(1000);
+    right_nums = Vec::with_capacity(1000);
+
+    for line in lines {
+        let (left_s, right_s) = line.split_once(SEPARATOR).unwrap();
+        let left = left_s.to_string().parse::<i32>().unwrap();
+        let right = right_s.to_string().parse::<i32>().unwrap();
+        left_nums.push(left);
+        right_nums.push(right);
+    }
+
+    sum = 0;
+    for n in left_nums {
+        sum += u32::try_from(right_nums.iter().filter(|i| **i == n).count()).unwrap()
+            * u32::try_from(n).unwrap();
+    }
+
+    println!("Part 2: the similarity score is {sum}");
 }
