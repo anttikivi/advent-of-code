1bac672e3dbaac3bc9254510e46edc63006a2fe0
Antti Kivi <antti@anttikivi.fi>
2024-12-06 19:35:18 +0200

    feat(2024): add placeholder for day 5

---
+++ 2024/src/day_05.rs
@@ -0,0 +1,4 @@
+pub fn run() {
+    println!("*** Advent of Code 2024 ***");
+    println!("--- Day 5: Print Queue  ---");
+}
--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -4,6 +4,7 @@ mod day_01;
 mod day_02;
 mod day_03;
 mod day_04;
+mod day_05;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,6 +19,7 @@ fn main() {
         2 => day_02::run(),
         3 => day_03::run(),
         4 => day_04::run(),
+        5 => day_05::run(),
         _ => println!("No solution for day {day_to_run}"),
     }
 }
